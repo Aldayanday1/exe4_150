815fab5b870c30f99ef8fba00124462010494065
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2022-12-09 16:48:13 +0700

    Change some the source code of the stack

--- exe4_150/Program.cs
+++ exe4_150/Program.cs
@@ -1,89 +1,139 @@
 ﻿using System;
-
-namespace StackImplementation
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ConsoleApplication1
 {
-    internal class Stack
-    {
-        static readonly int aldi = 610;
-        int top;
-        int[] stack = new int[aldi];
-
-        bool isEmpty()
-        {
-            return (top < 0);
-        }
-        public Stack()
-        {
-            top = -1;
-        }
-        internal bool Push(int data)
-        {
-            if (top >= aldi)
-            {
-                Console.WriteLine("Stack Overflow");
-                return false;
-            }
-            else
-            {
-                stack[++top] = data;
-                return true;
-            }
-        }
-        internal int Pop()
-        {
-            if (top < 0)
-            {
-                Console.WriteLine("Stack Underflow");
-                return 0;
-            }
-            else
-            {
-                int value = stack[top--];
-                return value;
-            }
-        }
-        internal void Peek()
-        {
-            if (top < 0)
-            {
-                Console.WriteLine("Stack Underflow");
-                return;
-            }
-            else
-                Console.WriteLine("The topmost element of stack is : {0}", stack[top]);
-        }
-        internal void PrintStack()
-        {
-            if (top < 0)
-            {
-                Console.WriteLine("Stack Underflow");
-                return;
-            }
-            else
-            {
-                Console.WriteLine("Items in the Stack are :");
-                for (int i = top; i > 610; i--)
-                {
-                    Console.WriteLine(stack[i]);
-                }
-            }
-        }
-    }
     class Program
     {
         static void Main(string[] args)
         {
-            Stack myStack = new Stack();
+            stack st = new stack();
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("\nStack MENU(size -- 10)");
+                Console.WriteLine("1. Add an element");
+                Console.WriteLine("2. See the Top element.");
+                Console.WriteLine("3. Remove top element.");
+                Console.WriteLine("4. Display stack elements.");
+                Console.WriteLine("5. Exit");
+                Console.Write("Select your choice: ");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Enter an Element : ");
+                        st.Push(Console.ReadLine());
+                        break;
 
-            myStack.Push(610);
-            myStack.Push(620);
-            myStack.Push(630);
-            myStack.Push(640);
-            myStack.PrintStack();
-            myStack.Peek();
-            Console.WriteLine("Item popped frpm Stack : {0}", myStack.Pop());
-            myStack.PrintStack();
+                    case 2:
+                        Console.WriteLine("Top element is: {0}", st.Peek());
+                        break;
 
+                    case 3:
+                        Console.WriteLine("Element removed: {0}", st.Pop());
+                        break;
+
+                    case 4:
+                        st.Display();
+                        break;
+
+                    case 5:
+                        System.Environment.Exit(1);
+                        break;
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+
+    interface StackADT
+    {
+        Boolean isEmpty();
+        void Push(Object element);
+        Object Pop();
+        Object Peek();
+        void Display();
+    }
+    class stack : StackADT
+    {
+        private int StackSize;
+        public int StackSizeSet
+        {
+            get { return StackSize; }
+            set { StackSize = value; }
+        }
+        public int top;
+        Object[] item;
+        public stack()
+        {
+            StackSizeSet = 10;
+            item = new Object[StackSizeSet];
+            top = -1;
+        }
+        public stack(int capacity)
+        {
+            StackSizeSet = capacity;
+            item = new Object[StackSizeSet];
+            top = -1;
+        }
+        public bool isEmpty()
+        {
+            if (top == -1) return true;
+
+            return false;
+        }
+        public void Push(object element)
+        {
+            if (top == (StackSize - 1))
+            {
+                Console.WriteLine("Stack is full!");
+            }
+
+            else
+            {
+
+                item[++top] = element;
+                Console.WriteLine("Item pushed successfully!");
+            }
+        }
+        public object Pop()
+        {
+            if (isEmpty())
+            {
+                Console.WriteLine("Stack is empty!");
+                return "No elements";
+            }
+            else
+            {
+
+                return item[top--];
+            }
+        }
+        public object Peek()
+        {
+            if (isEmpty())
+            {
+
+                Console.WriteLine("Stack is empty!");
+                return "No elements";
+            }
+            else
+            {
+                return item[top];
+            }
+        }
+
+
+        public void Display()
+        {
+            for (int i = top; i > -1; i--)
+            {
+
+                Console.WriteLine("Item {0}: {1}", (i + 1), item[i]);
+            }
         }
     }
 }
